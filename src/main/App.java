5f01b47f17f3610068992981becf5560ad603b18
Alicia Villaseñor Ormeño <95478866+aliciavo@users.noreply.github.com>
2025-12-04 10:30:53 +0100

    Integrate command line interface for date input

--- src/main/App.java
+++ src/main/App.java
@@ -1,17 +1,21 @@
+import es.uclm.esi.iso2.axx.command_Line_Interface.commandLine;
+
+import es.uclm.esi.iso2.axx.command_Line_Interface.commandLineI;
 public class App {
 
     public static void main(String[] args) {
-
+    	commandLine commL= new CommandLine();
         System.out.println("Hi, we are going to check wether a year is a leap year or not");
 
         try {
-            MyDate date = Keyboard.readDate();
+        	Date fulldate=commL.readDate()
+            MyDate date = new MyDate(fulldate.getDay(),fulldate.getMonth(),fulldate.getYear());
 
-            if (date.isLeapYear())
+            if (date.isLeapYear()) {
                 System.out.println("The year " + date.getYear() + " IS a leap year.");
-            else
+            }else {
                 System.out.println("The year " + date.getYear() + " is NOT a leap year.");
-
+            }
         } catch (InvalidFormatException | InvalidDateException e) {
             System.out.println("Error: " + e.getMessage());
         }
