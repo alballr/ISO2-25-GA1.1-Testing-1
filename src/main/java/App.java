3668bc9426884af31062da18cfbd0d3be45b9191
HIBA EL FARISSI <Hiba.Elfarissi@alu.uclm.es>
2025-12-16 16:22:10 +0100

    correcting code and putting it in java folder

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,26 @@
+
+import java.util.Calendar;
+import java.util.Date;
+
 import es.uclm.esi.iso2.axx.command_Line_Interface.commandLine;
 
-import es.uclm.esi.iso2.axx.command_Line_Interface.commandLineI;
 public class App {
 
-    public static void main(String[] args) {
-    	commandLine commL= new CommandLine();
+    public static void main(String[] args) throws InvalidFormatException {
+    	commandLine commL= new commandLine();
         System.out.println("Hi, we are going to check wether a year is a leap year or not");
 
         try {
-        	Date fulldate=commL.readDate()
-            MyDate date = new MyDate(fulldate.getDay(),fulldate.getMonth(),fulldate.getYear());
+        	Date fulldate=commL.readDate();
+            @SuppressWarnings("deprecation")
+			MyDate date = new MyDate(fulldate.getDay(),fulldate.getMonth()+1,fulldate.getYear()+1900);
 
             if (date.isLeapYear()) {
                 System.out.println("The year " + date.getYear() + " IS a leap year.");
             }else {
                 System.out.println("The year " + date.getYear() + " is NOT a leap year.");
             }
-        } catch (InvalidFormatException | InvalidDateException e) {
+        } catch (InvalidDateException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
--- src/main/java/MyDate.java
+++ src/main/java/MyDate.java
@@ -20,13 +20,18 @@ public class MyDate {
     }
 
     private int daysInMonth(int month, int year) {
-        return switch(month) {
-            case 1,3,5,7,8,10,12 -> 31;
-            case 4,6,9,11 -> 30;
-            case 2 -> isLeapYear(year) ? 29 : 28;
-            default -> throw new IllegalArgumentException();
-        };
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 4: case 6: case 9: case 11:
+                return 30;
+            case 2:
+                return isLeapYear(year) ? 29 : 28;
+            default:
+                return 0;
+        }
     }
+
 
     private boolean isLeapYear(int y) {
         return (y % 400 == 0) || (y % 4 == 0 && y % 100 != 0);
